8a0ce58b3a00b2c4e077d433d4a859ba31696f66
mjiang102628 <mjiang102628@gmail.com>
2019-08-13 10:25:09 -0400

    Create AlgoConverter.java

---
+++ src/main/java/com/algorand/algosdk/util/AlgoConverter.java
@@ -0,0 +1,31 @@
+package com.algorand.algosdk.util;
+
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+public class AlgoConverter {
+
+    private static BigDecimal ALGOS_TO_MICROALGOS_RATIO = BigDecimal.valueOf(1000000L);
+
+    /**
+     * Convert microalgos to algos.
+     * @param microalgos
+     * @return algos
+     */
+    public static BigDecimal toAlgos(BigInteger microalgos) {
+        BigDecimal algos = new BigDecimal(microalgos).divide(ALGOS_TO_MICROALGOS_RATIO);
+        return algos;
+    }
+
+    /**
+     * Convert algos to microalgos.
+     * @param algos
+     * @return microalgos
+     */
+    public static BigInteger toMicroAlgos(BigDecimal algos) {
+        BigDecimal microalgos = algos.multiply(ALGOS_TO_MICROALGOS_RATIO);
+        return microalgos.toBigInteger();
+    }
+
+}
