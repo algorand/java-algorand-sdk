2553d6de0d7d4e30755e93d66c73f0f0a19270a7
Will Winder <wwinder.unh@gmail.com>
2020-01-15 14:46:02 -0500

    Update javadoc with better usage information, add missing test goldens.

--- src/main/java/com/algorand/algosdk/templates/DynamicFee.java
+++ src/main/java/com/algorand/algosdk/templates/DynamicFee.java
@@ -128,6 +128,10 @@ public class DynamicFee {
      * Encoder.decodeFromMsgPack(encodedTxn, Transaction.class),
      * Encoder.decodeFromMsgPack(encodedLsig, LogicsigSignature.class),
      *
+     * Create the Transaction and LogicsigSignature objects from base64 encoded objects:
+     * Encoder.decodeFromMsgPack(encodedTxn, Transaction.class),
+     * Encoder.decodeFromMsgPack(encodedLsig, LogicsigSignature.class),
+     *
      * @param txn main transaction from payer
      * @param lsig signed logic received from payer
      * @param account an account initialized with a signing key.
