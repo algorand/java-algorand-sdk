8a4d4b939fa2ef8b69daf44cebea565b25b2a861
mjiang102628 <mjiang102628@gmail.com>
2020-03-11 16:59:08 -0400

    generalize config files setup

--- src/test/docker/sdk.py
+++ src/test/docker/sdk.py
@@ -17,9 +17,8 @@ import sys
 
 default_dirs = { 
     'features_dir': '/opt/java-algorand-sdk/src/test/resources/features',
-    'url': 'https://github.com/algorand/java-algorand-sdk.git',
     'source': '/opt/java-algorand-sdk',
-    'temp': '/opt/java-algorand-sdk/src/test/resources'
+    'docker': '/opt/java-algorand-sdk/src/test/docker'
 }
 
 version = "current"
@@ -29,14 +28,6 @@ def setup_sdk():
     """
     Setup java cucumber environment.
     """    
-    # cd $TRAVIS_BUILD_DIR
-    # mvn package -q -DskipTests
-    # ALGOSDK_VERSION=$(mvn -q -Dexec.executable=echo  -Dexec.args='${project.version}' --non-recursive exec:exec)
-    # cd - # back to 'java_cucumber'
-    # find "${TRAVIS_BUILD_DIR}/target" -type f -name "*.jar" -exec mvn install:install-file -q -Dfile={} -DpomFile="${TRAVIS_BUILD_DIR}/pom.xml" \;
-    # mvn versions:use-dep-version -DdepVersion=$ALGOSDK_VERSION -Dincludes=com.algorand:algosdk -DforceVersion=true -q
-    
-    # delete target
     subprocess.check_call(['rm -rf target'], shell=True)
     subprocess.check_call(['mvn install -q -DskipTests=true'], shell=True)
 
