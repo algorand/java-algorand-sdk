c17b6f5a2c3ba3c6384293e4484008a4715fe39c
mjiang102628 <mjiang102628@gmail.com>
2020-03-11 15:59:38 -0400

    Update run_docker.sh

--- src/test/docker/run_docker.sh
+++ src/test/docker/run_docker.sh
@@ -5,8 +5,6 @@ set -e
 rm -rf temp
 rm -rf src/test/resources/features
 
-# TODO have different branches contain different milestones; for example some branch might have the feature files for 
-# before templates so that tests don't fail even if templates have not been implemented in that sdk
 git clone --single-branch --branch michelle/test https://github.com/algorand/algorand-sdk-testing.git temp
 cp src/test/docker/sdk.py temp/docker
 mv temp/features src/test/resources/features
