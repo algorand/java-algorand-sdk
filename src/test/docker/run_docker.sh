a3f6b0c6271cb9747867353e5686db582595003b
mjiang102628 <mjiang102628@gmail.com>
2020-03-19 14:17:43 -0400

    Update run_docker.sh

--- src/test/docker/run_docker.sh
+++ src/test/docker/run_docker.sh
@@ -4,7 +4,7 @@ set -e
 
 rm -rf temp
 rm -rf src/test/resources/features
-git clone --single-branch --branch michelle/test https://github.com/algorand/algorand-sdk-testing.git temp
+git clone --single-branch --branch templates https://github.com/algorand/algorand-sdk-testing.git temp
 
 cp src/test/docker/sdk.py temp/docker
 mv temp/features src/test/resources/features
