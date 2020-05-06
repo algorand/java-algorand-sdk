074a6094bdf2cfadf0b6d0f2074d279c05ce3520
Will Winder <wwinder.unh@gmail.com>
2020-05-06 08:43:55 -0400

    Fix integration tests.

--- run_integration_tests.sh
+++ run_integration_tests.sh
@@ -7,12 +7,18 @@ pushd $rootdir
 
 # Reset test harness
 rm -rf test-harness
-git clone --single-branch --branch will/indexer https://github.com/algorand/algorand-sdk-testing.git test-harness
+git clone --single-branch --branch develop https://github.com/algorand/algorand-sdk-testing.git test-harness
+
+rm test-harness/features/integration/indexer.feature
+rm test-harness/features/unit/v2*feature
 
 ## Copy feature files into the project resources
+rm -rf src/test/resources/com/algorand/algosdk/integration
+rm -rf src/test/resources/com/algorand/algosdk/unit
 mkdir -p src/test/resources/com/algorand/algosdk/integration
-cp test-harness/features/integration/* src/test/resources/com/algorand/algosdk/integration
-cp test-harness/features/offline.feature src/test/resources/com/algorand/algosdk/integration
+mkdir -p src/test/resources/com/algorand/algosdk/unit
+cp -r test-harness/features/integration/* src/test/resources/com/algorand/algosdk/integration
+cp -r test-harness/features/unit/* src/test/resources/com/algorand/algosdk/unit
 
 # Build SDK testing environment
 docker build -t java-sdk-testing -f Dockerfile "$(pwd)"
