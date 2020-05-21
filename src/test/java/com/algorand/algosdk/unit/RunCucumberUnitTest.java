b1ee108986988b788f61d7bc982c2ec117ac4882
Will Winder <wwinder.unh@gmail.com>
2020-05-21 15:30:49 -0400

    Only one cucumber run class per package, disabled by default.

--- src/test/java/com/algorand/algosdk/unit/RunCucumberUnitTest.java
+++ src/test/java/com/algorand/algosdk/unit/RunCucumberUnitTest.java
@@ -5,6 +5,6 @@ import io.cucumber.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"progress"}, strict = true)
-public class RunCucumberTest {
+@CucumberOptions(plugin = {"progress"}, tags = "@disabled.by.default", strict = true)
+public class RunCucumberUnitTest {
 }
