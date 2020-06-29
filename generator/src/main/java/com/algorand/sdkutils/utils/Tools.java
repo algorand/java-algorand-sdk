fcb2aeff43a5dabcaed7f1f139948fcbcd1cc7ba
Pavel Zbitskiy <65323360+algorandskiy@users.noreply.github.com>
2020-06-29 07:46:41 -0400

    Improve go generator (#166)
    * Make go-lint compatible comments
    * Pass request method to Do generator

--- generator/src/main/java/com/algorand/sdkutils/utils/Tools.java
+++ generator/src/main/java/com/algorand/sdkutils/utils/Tools.java
@@ -1,27 +1,63 @@
 package com.algorand.sdkutils.utils;
 
+import java.lang.Character;
 import java.util.StringTokenizer;
 
 public class Tools {
 
-    // formatComment formats the comment by breaking lines, and incorporating 
-    // embedded formatting inside the comment. 
+    // formatComment formats the comment by breaking lines, and incorporating
+    // embedded formatting inside the comment.
     // If the comment is embedded inside another comment, full == false
     public static String formatComment(String comment, String tab, boolean full) {
         if (comment == null || comment.isEmpty()) {
             return "";
         }
         StringBuffer sb = new StringBuffer();
-
-        comment = comment.replace("\\[", "(");
-        comment = comment.replace("\\]", ")");
-        comment = comment.replace("\n", " __NEWLINE__ ");
         if (full) {
             sb.append(tab+"/**");
             sb.append("\n"+tab+" *");
         } else {
             sb.append(tab+" *");
         }
+
+        formatCommentImpl(sb, comment, " *", tab);
+
+        if (full) {
+            sb.append("\n"+tab+" */\n");
+        }
+        return sb.toString();
+    }
+
+    public static String formatCommentGo(String comment, String name, String tab) {
+        if (comment == null || comment.isEmpty()) {
+            return "";
+        }
+        String commentPossibleModified = comment;
+        if (name.length() > 0 && !comment.startsWith(name)) {
+            char firstChar = comment.charAt(0);
+            if (Character.isUpperCase(firstChar)) {
+                firstChar = Character.toLowerCase(firstChar);
+            }
+            commentPossibleModified = name + " " + firstChar + comment.substring(1);
+        }
+        int endIndex = commentPossibleModified.length();
+        if (commentPossibleModified.endsWith("\n") && endIndex > 2) {
+            commentPossibleModified = commentPossibleModified.substring(0, endIndex-1);
+        }
+        StringBuffer sb = new StringBuffer();
+        sb.append(tab+"//");
+        formatCommentImpl(sb, commentPossibleModified, "//", tab);
+        sb.append("\n");
+        return sb.toString();
+    }
+
+    private static void formatCommentImpl(
+        StringBuffer sb, String comment,
+        String comStr, String tab
+    ) {
+        comment = comment.replace("\\[", "(");
+        comment = comment.replace("\\]", ")");
+        comment = comment.replace("\n", " __NEWLINE__ ");
 
         StringTokenizer st = new StringTokenizer(comment);
         int line = 0;
@@ -32,24 +68,23 @@ public class Tools {
                     continue;
                 } else {
                     line = 0;
-                    sb.append("\n"+tab+" *");
+                    sb.append("\n"+tab+comStr);
                     continue;
                 }
             }
             if (line + token.length() > 80) {
                 line = 0;
-                sb.append("\n"+tab+" *");
+                sb.append("\n"+tab+comStr);
             }
-            token = token.replace('*', ' ');
+            if (comStr.indexOf("*") != -1) {
+                token = token.replace('*', ' ');
+            }
             sb.append(" ");
 
             sb.append(token);
             line += token.length() + 1;
         }
-        if (full) {
-            sb.append("\n"+tab+" */\n");
-        }
-        return sb.toString();
+        return;
     }
 
     public static String getCamelCase(String name, boolean firstCap) {
