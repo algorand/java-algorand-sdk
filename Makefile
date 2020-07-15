8a8b37566b9871adc3ca4b53425cb817235d3a6b
Pavel Zbitskiy <65323360+algorandskiy@users.noreply.github.com>
2020-07-15 08:28:28 -0400

    Implement TealSign and tests (#173)

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 unit:
-	mvn test -Dcucumber.filter.tags="@unit.offline or @unit.algod or @unit.indexer or @unit.rekey or @unit.indexer.rekey or @unit.transactions or @unit.responses or @unit.applications or @unit.dryrun"
+	mvn test -Dcucumber.filter.tags="@unit.offline or @unit.algod or @unit.indexer or @unit.rekey or @unit.indexer.rekey or @unit.transactions or @unit.responses or @unit.applications or @unit.dryrun or @unit.tealsign"
 
 integration:
 	mvn test -Dcucumber.filter.tags="@algod or @assets or @auction or @kmd or @send or @template or @indexer or @rekey or @applications or @compile or @dryrun"
---
+++ src/test/java/com/algorand/algosdk/unit/TealSign.java
@@ -0,0 +1,63 @@
+package com.algorand.algosdk.unit;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.security.NoSuchAlgorithmException;
+
+import com.algorand.algosdk.account.Account;
+import com.algorand.algosdk.crypto.Address;
+import com.algorand.algosdk.crypto.LogicsigSignature;
+import com.algorand.algosdk.crypto.Signature;
+import com.algorand.algosdk.util.Encoder;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class TealSign {
+    private Rekeying shared;
+    private byte[] data;
+    private Address contractAddress;
+    private Signature sig;
+
+    public TealSign(Rekeying shared) {
+        this.shared = shared;
+    }
+
+    @Given("base64 encoded data to sign {string}")
+    public void data_to_sign(String data) {
+        this.data = Encoder.decodeFromBase64(data);
+    }
+
+    @Given("base64 encoded private key {string}")
+    public void seed_for_private_key(String key) throws GeneralSecurityException {
+        byte[] seed = Encoder.decodeFromBase64(key);
+        shared.account = new Account(seed);
+    }
+
+    @Given("base64 encoded program {string}")
+    public void encoded_program(String program) throws GeneralSecurityException {
+        byte[] prog = Encoder.decodeFromBase64(program);
+        LogicsigSignature lsig = new LogicsigSignature(prog);
+        contractAddress = lsig.toAddress();
+    }
+
+    @Given("program hash {string}")
+    public void program_hash(String hash) throws GeneralSecurityException {
+        contractAddress = new Address(hash);
+    }
+
+    @When("I perform tealsign")
+    public void perform_teal_sign() throws NoSuchAlgorithmException, IOException {
+        sig = shared.account.tealSign(data, contractAddress);
+    }
+
+
+    @Then("the signature should be equal to {string}")
+    public void the_signature_should_equal_the_golden(String golden) throws IOException {
+        byte[] expected = Encoder.decodeFromBase64(golden);
+        assertThat(sig.getBytes()).isEqualTo(expected);
+    }
+}
