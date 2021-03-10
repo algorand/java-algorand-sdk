634a7c4bbe2227cf7ccc8e90f71b8b191cb557c7
Will Winder <wwinder.unh@gmail.com>
2021-03-10 16:44:10 -0500

    Fix base32 decode bug. (#210)

--- src/test/java/com/algorand/algosdk/util/TestEncoder.java
+++ src/test/java/com/algorand/algosdk/util/TestEncoder.java
@@ -1,15 +1,22 @@
 package com.algorand.algosdk.util;
 
-import org.junit.jupiter.api.Test;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class TestEncoder {
-    @Test
-    public void Base32() {
-        String txId = "3DV5VXT3QFRPVRTZTWP7PSGJN37BXJNXQWAFFW34OJWIZ2UOINQQ";
-        byte[] decoded = Encoder.decodeFromBase32StripPad(txId);
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "3DV5VXT3QFRPVRTZTWP7PSGJN37BXJNXQWAFFW34OJWIZ2UOINQQ",
+            "B3R46T7Z3JYHSZUMDKVQQKBYLJKWLJ36FQ5OJ3FFSWFXNXJ5HURQ",
+            "B3R46T7Z3JYHSZUMDKVQQKBYLJKWLJ36FQ5OJ3FFSWFXNXJ5HU======",
+            "B3R46T7Z3JYHSZUMDKVQQKBYLJKWLJ36FQ5OJ3FFSWFXNXJ5HU"
+    })
+    public void Base32EncodeDecode(String input) {
+        byte[] decoded = Encoder.decodeFromBase32StripPad(input);
         String reEncoded = Encoder.encodeToBase32StripPad(decoded);
-        assertThat(reEncoded).isEqualTo(txId);
+        assertThat(reEncoded).isEqualTo(StringUtils.stripEnd(input, "="));
     }
 }
