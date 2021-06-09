f7ab8aebcb433e43a9391db41a31d7ea436bbab6
Ayush Aggarwal <ayushaggarwal@berkeley.edu>
2021-06-09 17:21:20 -0400

    largeprograms testing, removed client-side validation for application calls

--- src/main/java/com/algorand/algosdk/transaction/AssetParams.java
+++ src/main/java/com/algorand/algosdk/transaction/AssetParams.java
@@ -99,7 +99,7 @@ public class AssetParams implements Serializable {
         }
 
         if(url != null) {
-            if (url.length() > 32) throw new RuntimeException("asset url cannot be greater than 32 characters");
+            if (url.length() > 96) throw new RuntimeException("asset url cannot be greater than 32 characters");
             this.url = url;
         }
 
