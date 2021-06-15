c4ac58b16d6922c847672e789d63e875a1b68bd5
Ayush Aggarwal <ayushaggarwal@berkeley.edu>
2021-06-15 19:17:13 -0400

    extraPages rent on app create

--- src/test/java/com/algorand/algosdk/cucumber/shared/TransactionSteps.java
+++ src/test/java/com/algorand/algosdk/cucumber/shared/TransactionSteps.java
@@ -42,7 +42,6 @@ public class TransactionSteps {
         throw new RuntimeException("Unknown error.");
     }
 
-    @When("I build an application transaction with operation {string}, application-id {long}, sender {string}, approval-program {string}, clear-program {string}, global-bytes {long}, global-ints {long}, local-bytes {long}, local-ints {long}, app-args {string}, foreign-apps {string}, foreign-assets {string} app-accounts {string}, fee {long}, first-valid {long}, last-valid {long}, genesis-hash {string}, extra-pages {long}")
     @When("I build an application transaction with operation {string}, application-id {long}, sender {string}, approval-program {string}, clear-program {string}, global-bytes {long}, global-ints {long}, local-bytes {long}, local-ints {long}, app-args {string}, foreign-apps {string}, foreign-assets {string}, app-accounts {string}, fee {long}, first-valid {long}, last-valid {long}, genesis-hash {string}, extra-pages {long}")
     public void buildApplicationTransactions(String operation, Long applicationId, String sender, String approvalProgramFile, String clearProgramFile, Long globalBytes, Long globalInts, Long localBytes, Long localInts, String appArgs, String foreignApps, String foreignAssets, String appAccounts, Long fee, Long firstValid, Long lastValid, String genesisHash, Long extraPages) {
         ApplicationBaseTransactionBuilder builder = null;
