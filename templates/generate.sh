eb16bc75551b9c5b1035c7685fbb9fdcc42e6934
Brice Rising <brice@algorand.com>
2021-07-23 11:20:28 -0400

    Use GENERATOR instead of TEMPLATE

--- templates/generate.sh
+++ templates/generate.sh
@@ -4,7 +4,8 @@ set -e
 curl -o indexer.json https://raw.githubusercontent.com/algorand/indexer/master/api/indexer.oas2.json
 curl -o algod.json https://raw.githubusercontent.com/algorand/go-algorand/rel/stable/daemon/algod/api/algod.oas2.json
 
-$TEMPLATE \
+$GENERATOR \
+       java \
        -c  "src/main/java/com/algorand/algosdk/v2/client/common" \
        -cp "com.algorand.algosdk.v2.client.common" \
        -m  "src/main/java/com/algorand/algosdk/v2/client/model" \
@@ -18,7 +19,8 @@ $TEMPLATE \
 
 # There is one enum only defined by indexer. Run this second to avoid
 # overwriting the second one.
-$TEMPLATE \
+$GENERATOR \
+       java \
        -c  "src/main/java/com/algorand/algosdk/v2/client/common" \
        -cp "com.algorand.algosdk.v2.client.common" \
        -m  "src/main/java/com/algorand/algosdk/v2/client/model" \
