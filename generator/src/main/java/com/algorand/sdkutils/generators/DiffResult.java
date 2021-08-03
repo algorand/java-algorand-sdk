5a1c00eca966cfa9518ff25e15e7847bf9fb7f23
Jack Smith <87339414+algojack@users.noreply.github.com>
2021-08-03 13:23:28 -0400

    Delete generator directory

--- generator/src/main/java/com/algorand/sdkutils/generators/DiffResult.java
+++
@@ -1,9 +0,0 @@
-package com.algorand.sdkutils.generators;
-
-public class DiffResult {
-
-    public String fullOutput;
-    public String justDiff;
-    public boolean pass;
-    public boolean filtered;
-}
