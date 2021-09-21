7873d5c0b58a00a43a8d8725085cea39e89ba898
Hang Su <hang.su@algorand.com>
2021-09-21 12:38:16 -0400

    remove unnecessary import

--- src/main/java/com/algorand/algosdk/account/Account.java
+++ src/main/java/com/algorand/algosdk/account/Account.java
@@ -14,7 +14,6 @@ import org.bouncycastle.asn1.ASN1Encodable;
 import org.bouncycastle.asn1.ASN1OctetString;
 import org.bouncycastle.asn1.pkcs.PrivateKeyInfo;
 
-import javax.annotation.Signed;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.math.BigInteger;
