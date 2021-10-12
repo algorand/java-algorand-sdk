8b800c1b4196ea89e5956e595f6ecb4e1cd58473
Hang Su <hang.su@algorand.com>
2021-10-12 11:25:18 -0400

    update msgpack version, something else happened

--- src/test/java/com/algorand/algosdk/client/TestClient.java
+++ src/test/java/com/algorand/algosdk/client/TestClient.java
@@ -1,7 +1,10 @@
 package com.algorand.algosdk.client;
 
 import com.algorand.algosdk.v2.client.common.AlgodClient;
+import com.algorand.algosdk.v2.client.common.Response;
+import com.algorand.algosdk.v2.client.model.BlockResponse;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -22,5 +25,20 @@ public class TestClient {
         } catch (Exception e) {
             Assertions.assertThat(expectError).isTrue();
         }
+    }
+
+    @Test
+    public void testEvalDelta() throws Exception {
+        String host = "https://academy-algod.dev.aws.algodev.network";
+        int port = 443;
+        String token = "2f3203f21e738a1de6110eba6984f9d03e5a95d7a577b34616854064cf2c0e7b";
+        AlgodClient client = new AlgodClient(host, port, token);
+
+        long round = 17162527;
+
+        Response<BlockResponse> response = client.GetBlock(round).execute();
+        // System.out.println(Encoder.encodeToBase64(response.getRawResponse()));
+        Throwable throwable = Assertions.catchThrowable(() -> response.toString());
+        Assertions.assertThat(throwable).isNull();
     }
 }
