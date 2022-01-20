250d5a02e1e3410006f0b26d9583f616fac0cdf2
Hang Su <hang.su@algorand.com>
2022-01-20 15:06:54 -0500

    minor, remove commented code in abiType

--- src/main/java/com/algorand/algosdk/abi/ABIType.java
+++ src/main/java/com/algorand/algosdk/abi/ABIType.java
@@ -161,27 +161,6 @@ public abstract class ABIType {
         return until;
     }
 
-//    /**
-//     * Given an array/list-like object, infer an array of objects
-//     * @param val an array/list-like object
-//     * @return inferred array of objects
-//     * @throws IllegalArgumentException if it cannot infer if the object is list or array
-//     */
-//    protected static Object[] unifyToArrayOfObjects(Object val) {
-//        if (val.getClass().isArray()) {
-//            if (val instanceof Object[])
-//                return (Object[]) val;
-//            int length = Array.getLength(val);
-//            Object[] outputArray = new Object[length];
-//            for (int i = 0; i < length; ++i)
-//                outputArray[i] = Array.get(val, i);
-//            return outputArray;
-//        } else if (val instanceof List<?>)
-//            return ((List<?>) val).toArray(new Object[0]);
-//        else
-//            throw new IllegalArgumentException("cannot infer type for unify array/list-like object to object array");
-//    }
-
     /**
      * Take the first 2 bytes in the byte array
      * (consider the byte array to be an encoding for ABI dynamic typed value)
