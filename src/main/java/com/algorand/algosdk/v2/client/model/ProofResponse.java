cf48c6496de19c8aa0230c038755fc5531172f3e
Or Aharonee <or.aharonee@algorand.com>
2022-05-03 18:39:45 +0300

    return hashtype in ProofResponse

--- src/main/java/com/algorand/algosdk/v2/client/model/ProofResponse.java
+++ src/main/java/com/algorand/algosdk/v2/client/model/ProofResponse.java
@@ -12,6 +12,14 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public class ProofResponse extends PathResponse {
 
     /**
+     * The type of hash function used to create the proof, must be one of:
+     *   sha512_256
+     *   sha256
+     */
+    @JsonProperty("hashtype")
+    public Enums.Hashtype hashtype;
+
+    /**
      * Index of the transaction in the block's payset.
      */
     @JsonProperty("idx")
@@ -55,6 +63,7 @@ public class ProofResponse extends PathResponse {
         if (o == null) return false;
 
         ProofResponse other = (ProofResponse) o;
+        if (!Objects.deepEquals(this.hashtype, other.hashtype)) return false;
         if (!Objects.deepEquals(this.idx, other.idx)) return false;
         if (!Objects.deepEquals(this.proof, other.proof)) return false;
         if (!Objects.deepEquals(this.stibhash, other.stibhash)) return false;
