b101fbc2b33b9b0c5aa1aab418d34c6397c305c9
Eric Warehime <eric.warehime@gmail.com>
2022-05-24 12:19:04 -0700

    Rerun code generation based on new models (#321)

---
+++ src/main/java/com/algorand/algosdk/v2/client/model/DisassembleResponse.java
@@ -0,0 +1,30 @@
+package com.algorand.algosdk.v2.client.model;
+
+import java.util.Objects;
+
+import com.algorand.algosdk.v2.client.common.PathResponse;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * Teal disassembly Result
+ */
+public class DisassembleResponse extends PathResponse {
+
+    /**
+     * disassembled Teal code
+     */
+    @JsonProperty("result")
+    public String result;
+
+    @Override
+    public boolean equals(Object o) {
+
+        if (this == o) return true;
+        if (o == null) return false;
+
+        DisassembleResponse other = (DisassembleResponse) o;
+        if (!Objects.deepEquals(this.result, other.result)) return false;
+
+        return true;
+    }
+}
