161d4fae2fe85c7ea1421d4b504fc7fc72c7e3e2
Jason Paulos <jasonpaulos@users.noreply.github.com>
2022-05-24 09:21:24 -0700

    Remove unnecessary checks

--- src/main/java/com/algorand/algosdk/transaction/AtomicTransactionComposer.java
+++ src/main/java/com/algorand/algosdk/transaction/AtomicTransactionComposer.java
@@ -245,15 +245,9 @@ public class AtomicTransactionComposer {
         tx.onCompletion = methodCall.onCompletion;
         tx.approvalProgram = methodCall.approvalProgram;
         tx.clearStateProgram = methodCall.clearProgram;
-
-        if (methodCall.globalStateSchema != null)
-            tx.globalStateSchema = methodCall.globalStateSchema;
-
-        if (methodCall.localStateSchema != null)
-            tx.localStateSchema = methodCall.localStateSchema;
-
-        if (methodCall.extraPages != null)
-            tx.extraPages = methodCall.extraPages;
+        tx.globalStateSchema = methodCall.globalStateSchema;
+        tx.localStateSchema = methodCall.localStateSchema;
+        tx.extraPages = methodCall.extraPages;
 
         this.transactionList.addAll(tempTransWithSigner);
         this.transactionList.add(new TransactionWithSigner(tx, methodCall.signer));
