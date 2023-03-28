8022aae33dd68d67221e3a07c14bb3f1148c0ac7
nullun <git@nullun.com>
2023-03-28 15:55:07 +0100

    Fix the AtomicGroup example

--- examples/smoke_test.sh
+++ examples/smoke_test.sh
@@ -5,16 +5,18 @@ export INDEXER_PORT="59999"
 export KMD_PORT="60001"
 
 mvn clean compile assembly:single
-java -cp target/sdk-extras-1.0-SNAPSHOT-jar-with-dependencies.jar com.algorand.examples.ASAExamples
-java -cp target/sdk-extras-1.0-SNAPSHOT-jar-with-dependencies.jar com.algorand.examples.ATC
-java -cp target/sdk-extras-1.0-SNAPSHOT-jar-with-dependencies.jar com.algorand.examples.AccountExamples
-java -cp target/sdk-extras-1.0-SNAPSHOT-jar-with-dependencies.jar com.algorand.examples.AppExamples
-java -cp target/sdk-extras-1.0-SNAPSHOT-jar-with-dependencies.jar com.algorand.examples.AtomicTransfers
-java -cp target/sdk-extras-1.0-SNAPSHOT-jar-with-dependencies.jar com.algorand.examples.CodecExamples
-java -cp target/sdk-extras-1.0-SNAPSHOT-jar-with-dependencies.jar com.algorand.examples.Debug
-java -cp target/sdk-extras-1.0-SNAPSHOT-jar-with-dependencies.jar com.algorand.examples.IndexerExamples
-java -cp target/sdk-extras-1.0-SNAPSHOT-jar-with-dependencies.jar com.algorand.examples.KMDExamples
-java -cp target/sdk-extras-1.0-SNAPSHOT-jar-with-dependencies.jar com.algorand.examples.LSig
-java -cp target/sdk-extras-1.0-SNAPSHOT-jar-with-dependencies.jar com.algorand.examples.Overview
-java -cp target/sdk-extras-1.0-SNAPSHOT-jar-with-dependencies.jar com.algorand.examples.Participation
 
+# Loop over all files in the directory
+for file in $(find . -name "*.java" -type f | sort -n); do
+    # Check if the filename is not "ExampleUtils.java"
+    if [[ $file != *"ExampleUtils.java" ]]; then
+        # Remove the "./src/main/java/" from the file path
+        file="${file//.\/src\/main\/java\//}"
+        # Replace the "/" with "."
+        file="${file//\//.}"
+        # Remove the ".java" from the file path
+        file="${file//.java/}"
+        # Run the example
+        java -cp target/sdk-extras-1.0-SNAPSHOT-jar-with-dependencies.jar $file
+    fi
+done
